fc63bed0d85572aec48b789aaffedb0d3eb01ec0
Keverson-Teodoro <keversonteodoro55@gmail.com>
2025-08-31 01:56:39 -0300

    colocando a pasta do back-end no projeto

---
+++ remio/src/test/java/projetos/kev/remio/RemioApplicationTests.java
@@ -0,0 +1,13 @@
+package projetos.kev.remio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
