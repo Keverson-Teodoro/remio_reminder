895c9f59a912da6be7b87813125c2d79a9abd82c
Keverson-Teodoro <keversonteodoro55@gmail.com>
2025-08-31 02:07:48 -0300

    criando a entidade dos lemretes e implementando a estrutura de pastas padrão spring no back-end

---
+++ back-end/remio/src/main/java/projetos/kev/remio/model/entity/Reminder.java
@@ -0,0 +1,26 @@
+package projetos.kev.remio.model.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "reminders")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reminder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "description")
+    private String description;
+
+}
