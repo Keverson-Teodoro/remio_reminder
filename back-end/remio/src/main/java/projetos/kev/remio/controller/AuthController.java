75007eda9ec390ffd7de35de801513482ba0e0d5
Keverson-Teodoro <keversonteodoro55@gmail.com>
2025-09-02 23:42:09 -0300

    iniciando dependencia do spring security no pom xml

---
+++ back-end/remio/src/main/java/projetos/kev/remio/controller/AuthController.java
@@ -0,0 +1,19 @@
+package projetos.kev.remio.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import projetos.kev.remio.DTO.UserRegisterDTO;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register (@RequestBody UserRegisterDTO userRegisterDTO){
+        return null;
+    }
+}
