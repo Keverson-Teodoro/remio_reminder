3e2b6ffbd0cc849a4b12c43f78d1254f2fcb8863
Keverson-Teodoro <keversonteodoro55@gmail.com>
2025-09-09 20:55:30 -0300

    armazenando o token retornado pelo back-end no localstorage e mandando ele nas requisições feitas

--- front-end/remio_scrypt.js
+++ front-end/remio_scrypt.js
@@ -33,16 +33,18 @@
 
 function salvar(){
        
-        const url = "http://localhost:8080/reminder"
+        const url = "http://localhost:8088/reminder"
 
         var description = window.prompt("Novo lembrete");
+        var token = localStorage.getItem("token");
         
         
         try {
         const resposta = fetch(url, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
             },
             body: JSON.stringify({
                 description: description
@@ -65,19 +67,31 @@ function salvar(){
 }
 
 
+
+
 async function mostrar(){
 
     
-        const url = "http://localhost:8080/reminder"
+        const url = "http://localhost:8088/reminder"
+
+        const token = localStorage.getItem("token")
+
+        // if(token == null){
+        //     window.location.href = "remio_login_page.html"
+            
+        // }
+
+
     
     // $("#descricao").val("");
 
 
-        try {
+        try {           
         const resposta = await fetch(url, {
             method: "GET",
             headers: {
-                "Content-Type": "text/plain"
+                "Content-Type": "text/plain",
+                "Authorization": `Bearer ${token}` 
             }
         });
     
@@ -96,7 +110,7 @@ async function mostrar(){
             const novalinha = `
             <tr>
                 <td> <button class="remover" onClick="removeReminder('${element.id}')"></button> </td>
-                <td><input class="input-lembrete" value="${element.description ?? ''}" /></td>
+                <td><input id="reminder-input" class="input-lembrete" value="${element.description ?? ''}" /></td>
             </tr>
                 
         `;
@@ -113,18 +127,25 @@ async function mostrar(){
 
 }
 
+
+
+
+
 window.onload = mostrar;
+
 
 
 async function removeReminder(id){
 
     const url = `http://localhost:8080/reminder/${id}`;
+    const token = localStorage.getItem("token")
 
     try{
         const request = await fetch(url, {
             method: "DELETE",
             headers: {
-                "Content-Type": "text/plain"
+                "Content-Type": "text/plain",
+                "Authorization": `Bearer ${token}`
             }
             
 
@@ -146,36 +167,138 @@ async function removeReminder(id){
 
 }
 
-async function editReminder(id, obj) {
-    
+
+async function loginPage(){
+    const url = "localhost:8080/login-view"
+
+
+    try{
+
+        const respsonse = await fetch(url, {
+            method: "GET"
+        })
+
+
+        if(respsonse.ok){
+            console.log("Sucesso")
+        }
+    }catch(erro){
+        console.log("erro")
+    }
+
+
 }
 
 
 
 
-function cadastrar(){
+
+async function editReminder(id, description) {
+    const url = `http://localhost:8080/reminder/${id}`;
+
+
+    try{
+        const response = await fetch(url, {
+            method: "PATCH",
+            headers: "application/json",
+            body : JSON.stringify(description)
+        
+        })
+
+    }catch(erro){
+        console.log("erro")
+    }
+
+    
+}
+
+
+
+async function logar(){
+  
+    const url = "http://localhost:8088/auth/login";
+
+
+
+
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+
+
+    const authentication = {
+        username: username,
+        password: password
+    };
+
+    try{
+
+        const response = await fetch(url,  {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+
+            
+                
+            },
+            body : JSON.stringify(authentication)
+            
+    
+        })
+
+
+        if(response.ok){
+
+            const data = await response.json();
+            const token = data.token;
+    
+            localStorage.setItem("token", token)
+            window.location.href = "remio_home_page.html"
+        
+        }
+
+
+
+        
+    }catch(Erro){
+        console.log("erro ao mandar os dados")
+    }
+
+
+
+    // .then((resp) =>  console.log(resp.json))
+    // .catch((erro) => console.log('Deu ruim', erro))
+
+
+
+}
+
+
+
+
+async function cadastrar(){
+    
+    const url = "http://localhost:8088/auth/register";
+    
 
     const dados = {
         // nome: document.getElementById("nome").value,
-        nome: document.getElementById("username").value,
-        senha: document.getElementById("password").value
-        // email: document.getElementById("email").value,
+        username: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
         
 
     };
 
-    const url = "http://127.0.1.1:8080/users";
-
-    
 
     try {
-        const resposta = fetch(url, {
+        const resposta = await fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(dados)
         });
+
 
         if (!resposta.ok) {
             throw new Error("Erro no cadastro");
@@ -192,15 +315,9 @@ function cadastrar(){
 
 
 
-    // fetch(url, {
-    //     method: "POST",
-    //     headers:{
-    //         "Content-Type": "application/json"
-    //     },
-    //     // body: JSON.stringify({'nome': $("#nome").val(), 'email':$("#email").val(), 'senha': $("#senha").val()})
-        
-    // })
- 
+
+
+
     
 }
 
